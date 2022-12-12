8eaf74537cbac3d324ac5d962974374b8cec10ee
Arborym <dparizher@gmail.com>
2022-12-12 12:59:18 -0500

    Add StandardCommands Documentation

--- src/standard_commands.py
+++ src/standard_commands.py
@@ -8,23 +8,25 @@ from EmbedBuilder import EmbedBuilder
 
 
 class StandardCommands(commands.Cog):
-    def __init__(self, bot: commands.Bot, db_connection, cursor) -> None:
+    def __init__(
+        self, bot: commands.Bot, db_connection: object, cursor: object
+    ) -> None:
         self.bot = bot
         self.db_connection = db_connection
         self.cursor = cursor
 
     @commands.slash_command(name="ping", description="Check the bot's latency")
     async def ping(self, ctx: commands.Context) -> None:
-        start: float = time.perf_counter()
+        start = time.perf_counter()
         self.db_connection.ping()
         cursor = self.db_connection.cursor()
         cursor.execute("SELECT * FROM ping")
         cursor.fetchall()
         self.db_connection.commit()
-        end: float = time.perf_counter()
+        end = time.perf_counter()
         # Calculate database latency
-        latency: int = round((end - start) * 1000)
-        network_latency: float = round(measure_latency(host="google.com")[0], 2)
+        latency = round((end - start) * 1000)
+        network_latency = round(measure_latency(host="google.com")[0], 2)
         embed = EmbedBuilder(
             "Ping",
             f"**Database Latency:** {latency}ms\n**Network Latency:** {network_latency}ms",
